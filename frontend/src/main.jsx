555701bb91d0035dbdd712ac7bf0c36aba395b9d
eCodeVoyager <mu23172@gmail.com>
2024-10-05 03:47:02 +0600

    Import fixed in  main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import RouterApp from "./router";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./contexts/AuthContext";
-import { UserProvider } from "./contexts/userContext";
+import { UserProvider } from "./contexts/UserContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
