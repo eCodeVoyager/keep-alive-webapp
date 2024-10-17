586cab4615bc5080693948d0fb8ad8e6d3def71f
eCodeVoyager <mu23172@gmail.com>
2024-10-18 00:41:51 +0600

    fix loader repeat count

--- frontend/src/components/Shared/Loader.jsx
+++ frontend/src/components/Shared/Loader.jsx
@@ -4,7 +4,7 @@ import { Loader as LucideLoader } from "lucide-react";
 const Loader = () => {
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
-      <motion.div animate={{ rotate: 360 }}>
+      <motion.div animate={{ rotate: 360, repeatCount: Infinity }}>
         <LucideLoader className="h-16 w-16 text-green-500" />
       </motion.div>
     </div>
