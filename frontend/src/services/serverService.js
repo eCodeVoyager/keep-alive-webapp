5265f9d03a850b57f24db7d7868bb4c1d1269341
eCodeVoyager <mu23172@gmail.com>
2024-10-17 22:54:36 +0600

    Fix Server service api route

--- frontend/src/services/serverService.js
+++ frontend/src/services/serverService.js
@@ -2,7 +2,7 @@ import requests from "./httpRequest";
 
 const ServerService = {
   createServer: async (body) => requests.post("/websites", body),
-  getAllServers: async () => requests.get("/websites"),
+  getAllServers: async () => requests.get("/websites/me"),
   deleteServer: async (id) => requests.delete(`/websites/${id}`),
 };
 
