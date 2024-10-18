3f28799fb8a7799b30cb53b25e62162f132f8a6e
eCodeVoyager <mu23172@gmail.com>
2024-10-19 03:04:48 +0600

    Auth Validator:: Added name for user

--- backend/src/modules/auth/validations/authValidation.js
+++ backend/src/modules/auth/validations/authValidation.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 
 const register = {
   body: Joi.object().keys({
+    name: Joi.string().required(),
     email: Joi.string()
       .email({
         minDomainSegments: 2,
